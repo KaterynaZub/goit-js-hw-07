cfb5042981cecca01e90ae72d6ea189cafb7005f
KaterynaZub <katekornzub@gmail.com>
2024-02-18 20:47:56 +0100

    apdate task 2

--- js/task-2.js
+++ js/task-2.js
@@ -12,33 +12,27 @@ const images = [
       alt: 'Group of Horses Running',
     },
     {
-      url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      url:
+        'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       alt: 'Elephant Beside on Baby Elephant',
     },
     {
-      url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      url:
+        'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       alt: 'Blue Geeen and Orange Parrot',
     },
     {
-      url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+      url:
+        'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
       alt: 'Zebras on Zebra',
     },
   ];
-  const gallery = document.querySelector('.gallery');
+  const galleryList = document.querySelector('.gallery');
   
-  const fragment = document.createDocumentFragment();
+  const galleryHTML = images.map(image => `
+    <li class="gallery-item">
+      <img src="${image.url}" alt="${image.alt}" class="gallery-img">
+    </li>
+  `).join('');
   
-  images.forEach(image => {
-    const listItem = document.createElement('li');
-    const img = document.createElement('img');
-    img.src = image.url;
-    img.alt = image.alt;
-    img.style.width = '300px';
-    img.style.height = '180px';
-    img.style.padding = '10px';
-  
-    listItem.appendChild(img);
-    fragment.appendChild(listItem);
-  });
-  
-  gallery.appendChild(fragment);
+  galleryList.innerHTML = galleryHTML;
